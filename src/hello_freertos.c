3e7d1ed140f2094c4d5cad76716f7f9b14bbd13d
Matthew Morales <mtthwmrls9@gmail.com>
2024-11-04 12:45:55 -0700

    Initial setup

--- src/hello_freertos.c
+++ src/hello_freertos.c
@@ -8,37 +8,38 @@
 
 #include "FreeRTOS.h"
 #include "task.h"
+#include "semphr.h"
 
 #include "pico/stdlib.h"
 #include "pico/multicore.h"
 #include "pico/cyw43_arch.h"
 
-int count = 0;
-bool on = false;
+#define SUPERVISOR_TASK_PRIORITY      ( tskIDLE_PRIORITY + 3UL )
+#define SUPERVISOR_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 
-#define MAIN_TASK_PRIORITY      ( tskIDLE_PRIORITY + 1UL )
-#define BLINK_TASK_PRIORITY     ( tskIDLE_PRIORITY + 2UL )
-#define MAIN_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
-#define BLINK_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
+#define LOW_TASK_PRIORITY     ( tskIDLE_PRIORITY + 1UL )
+#define LOW_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 
-void blink_task(__unused void *params) {
-    hard_assert(cyw43_arch_init() == PICO_OK);
-    while (true) {
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
-        if (count++ % 11) on = !on;
-        vTaskDelay(500);
-    }
+#define HIGH_TASK_PRIORITY     ( tskIDLE_PRIORITY + 2UL )
+#define HIGH_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
+
+typedef struct PIData {
+
+};
+
+SemaphoreHandle_t semaphore;
+
+
+void high_task (__unused void *params) {
+    
 }
 
-void main_task(__unused void *params) {
-    xTaskCreate(blink_task, "BlinkThread",
-                BLINK_TASK_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL);
-    char c;
-    while(c = getchar()) {
-        if (c <= 'z' && c >= 'a') putchar(c - 32);
-        else if (c >= 'A' && c <= 'Z') putchar(c + 32);
-        else putchar(c);
-    }
+void low_task (__unused void *params) {
+
+}
+
+void supervisor_task(__unused void *params) {
+
 }
 
 int main( void )
@@ -47,8 +48,11 @@ int main( void )
     const char *rtos_name;
     rtos_name = "FreeRTOS";
     TaskHandle_t task;
-    xTaskCreate(main_task, "MainThread",
-                MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &task);
+
+    semaphore = xSemaphoreCreateBoolean();
+
+    xTaskCreate(supervisor_task, "MainThread",
+                SUPERVISOR_TASK_STACK_SIZE, NULL, SUPERVISOR_TASK_PRIORITY, &task);
     vTaskStartScheduler();
     return 0;
 }
