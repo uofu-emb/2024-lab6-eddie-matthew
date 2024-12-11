3785d8befa42dd89aca6f40cb2000a6da7c58462
Matthew Morales <mtthwmrls9@gmail.com>
2024-12-10 20:00:24 -0700

    Format activity 0 as a test

--- test/test.c
+++ test/test.c
@@ -20,96 +20,96 @@
 #define HIGH_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 
 SemaphoreHandle_t semaphore;
-QueueHandle_t priority_level_msg_queue;
+SemaphoreHandle_t high_task_completion_flag;
 
 TaskHandle_t supervisor_task_handle;
 TaskHandle_t low_task_handle;
 TaskHandle_t high_task_handle;
 TaskHandle_t medium_task_handle;
 
-void take_and_message (char level) {
-    xSemaphoreTake(semaphore, portMAX_DELAY);
-    xQueueSendToBack(priority_level_msg_queue, level, portMAX_DELAY);
-}
-
 void high_task (__unused void *params) {
     vTaskDelay(portTICK_PERIOD_MS*100);
-    take_and_message('H');
+    xSemaphoreTake(semaphore, portMAX_DELAY);
+    xSemaphoreGive(high_task_completion_flag);
     while (1) {};
 }
 
 void medium_task (__unused void *params) {    
     vTaskDelay(portTICK_PERIOD_MS*200);
-    busy_wait_ms(10000);
+    busy_wait_ms(3000);
     vTaskDelay(portTICK_PERIOD_MS*5000);
     while (1) {};
 }
 
 void low_task (__unused void *params) {
-    take_and_message('L');
+    bool res = xSemaphoreTake(semaphore, 100);
     int i = 0;
     busy_wait_ms(200);
     xSemaphoreGive(semaphore);
     while (1) {};
 }
 
-void supervisor_task(__unused void *params) {
-    char semphr_owner;
-    xQueueReceive(priority_level_msg_queue, &semphr_owner, portMAX_DELAY);
-    TEST_ASSERT_EQUAL_CHAR('L', semphr_owner);
-    xQueueReceive(priority_level_msg_queue, &semphr_owner, portMAX_DELAY);
-    TEST_ASSERT_EQUAL_CHAR('H', semphr_owner);
-    TickType_t time = xTaskGetTickCount();
-    TEST_ASSERT_GREATER_THAN_UINT32(10000*portTICK_PERIOD_MS, time);
+uint32_t induce_priority_inversion () {
+    TaskHandle_t high;
+    TaskHandle_t med;
+    TaskHandle_t low;
+    high_task_completion_flag = xSemaphoreCreateCounting(1, 0);
+
+    TickType_t start_time = xTaskGetTickCount();
+    xTaskCreate(low_task, "LOW", LOW_TASK_STACK_SIZE, NULL, LOW_TASK_PRIORITY, &low);
+    xTaskCreate(medium_task, "MED", MEDIUM_TASK_STACK_SIZE, NULL, MEDIUM_TASK_PRIORITY, &med);
+    xTaskCreate(high_task, "HIGH", HIGH_TASK_STACK_SIZE, NULL, HIGH_TASK_PRIORITY, &high);
+
+    xSemaphoreTake(high_task_completion_flag, portMAX_DELAY);
+
+    vSemaphoreDelete(high_task_completion_flag);
+
+    TickType_t end_time = xTaskGetTickCount();
+
+    uint32_t elapsed = (end_time - start_time)*portTICK_PERIOD_MS;
+
+    vTaskDelete(high);
+    vTaskDelete(med);
+    vTaskDelete(low);
+
+    return elapsed;
 }
 
-void test_priority_inversion_binary()
-{
+void test_priority_inversion_binary() {
     semaphore = xSemaphoreCreateBinary();
     xSemaphoreGive(semaphore);
-    priority_level_msg_queue = xQueueCreate(10, sizeof(char));
+    uint32_t elapsed_ms = induce_priority_inversion();
 
-    xTaskCreate(supervisor_task, "SupervisorThread",
-                SUPERVISOR_TASK_STACK_SIZE, NULL, SUPERVISOR_TASK_PRIORITY, &supervisor_task_handle);
-    xTaskCreate(low_task, "LowThread",
-                LOW_TASK_STACK_SIZE, NULL, LOW_TASK_PRIORITY, &low_task_handle);
-    xTaskCreate(medium_task, "MediumThread",
-                MEDIUM_TASK_STACK_SIZE, NULL, MEDIUM_TASK_PRIORITY, &medium_task_handle);
-    xTaskCreate(high_task, "HighThread",
-                HIGH_TASK_STACK_SIZE, NULL, HIGH_TASK_PRIORITY, &high_task_handle); 
-    
-    vTaskStartScheduler();
+    TEST_ASSERT_GREATER_THAN_UINT32(3000, elapsed_ms);
+
+    vSemaphoreDelete(semaphore);
 }
 
-void test_priority_inversion_mutex () {
-    semaphore = xSemaphoreCreateMutex();
-    xSemaphoreGive(semaphore);
-    priority_level_msg_queue = xQueueCreate(10, sizeof(char));
+void tearDown(void) {
 
-    xTaskCreate(supervisor_task, "SupervisorThread",
-                SUPERVISOR_TASK_STACK_SIZE, NULL, SUPERVISOR_TASK_PRIORITY, &supervisor_task_handle);
-    xTaskCreate(low_task, "LowThread",
-                LOW_TASK_STACK_SIZE, NULL, LOW_TASK_PRIORITY, &low_task_handle);
-    xTaskCreate(medium_task, "MediumThread",
-                MEDIUM_TASK_STACK_SIZE, NULL, MEDIUM_TASK_PRIORITY, &medium_task_handle);
-    xTaskCreate(high_task, "HighThread",
-                HIGH_TASK_STACK_SIZE, NULL, HIGH_TASK_PRIORITY, &high_task_handle); 
-    
-    vTaskStartScheduler();
+}
+void setUp(void) {
+
 }
 
-void setUp(void) {}
 
-void tearDown(void) {}
+void main_thread (__unused void *params) {
+    while (1) {
+        printf("Start tests\n");
+        UNITY_BEGIN();
+        RUN_TEST(test_priority_inversion_binary);
+        sleep_ms(10000);
+        UNITY_END();
+    }
+}
 
 int main (void)
 {
     stdio_init_all();
     sleep_ms(5000); // Give time for TTY to attach.
-    printf("\nStart tests\n");
-    UNITY_BEGIN();
-    RUN_TEST(test_priority_inversion_binary);
-    // RUN_TEST(test_priority_inversion_mutex);
-    sleep_ms(5000);
-    return UNITY_END();
+
+    TaskHandle_t main_handle;
+    xTaskCreate(main_thread, "MainThread",
+                configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY+10, &main_handle);
+    vTaskStartScheduler();
 }
